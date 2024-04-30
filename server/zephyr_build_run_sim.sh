77182ae7b96bee5b8322380225b57bd980119a8f
Jonas Remmert <jremmert@gmx.net>
2024-04-30 15:19:31 +0200

    Zephyr: simplify starting the simulation
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/zephyr_build_run_sim.sh
+++ server/zephyr_build_run_sim.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#
+# Start networking via net-tools
+../../tools/net-tools/net-setup.sh &
 
 ## Build and run Zephyr
-west build -b qemu_x86 ../../zephyr/samples/net/lwm2m_client -p -- -DCONF=overlay-lwm2m-1.1.conf
+west build -b qemu_x86 ../../zephyr/samples/net/lwm2m_client -- -DCONF=overlay-lwm2m-1.1.conf
 west build -t run
