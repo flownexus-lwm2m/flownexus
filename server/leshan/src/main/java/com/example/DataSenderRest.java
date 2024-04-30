76d3bc39d86e3392dfed8114bda16d3e48fa65e0
Jonas Remmert <jremmert@gmx.net>
2024-04-30 15:20:44 +0200

    RestSender: Choose target IP address based on execution mode
    If Django and Leshan run local and native, send to localhost.
    
    If everything runs dockerized, the django container is the target.
    
    This commit configures the ip address based on a environment variable.
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/leshan/src/main/java/com/example/DataSenderRest.java
+++ server/leshan/src/main/java/com/example/DataSenderRest.java
@@ -3,7 +3,6 @@ package com.example;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
 
@@ -14,23 +13,34 @@ public class DataSenderRest {
 
     private static final Logger logger = LoggerFactory.getLogger(DataSenderRest.class);
     private HttpClient client;
+    private static final String BASE_URI = System.getenv("DATA_SENDER_URI");
 
     public DataSenderRest() {
         client = HttpClient.newHttpClient();
     }
 
     public void sendData(String data) {
-        logger.info("DataSenderRest: {}", data);
+        String uri = (BASE_URI != null ? BASE_URI : "http://localhost:8000") +
+                                                    "/api/endpointdata/";
+
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://django:8000/api/endpointdata/"))
+                .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(data)) // Construct a request with the provided data
+                .POST(BodyPublishers.ofString(data))
                 .build();
 
-        System.out.println("DataSenderRest: " + data);
+        logger.debug("DataSenderRest: {}", data);
+
         client.sendAsync(request, BodyHandlers.ofString())
-              .thenApply(HttpResponse::body)
-              .thenAccept(responseBody -> logger.info("Successful response: {}", responseBody))
+              .thenApply(response -> {
+                  if (response.statusCode() >= 200 && response.statusCode() < 300) {
+                      return response.body();
+                  } else {
+                      logger.error("Request failed with: {}", response.statusCode());
+                      logger.error("Payload: {}", data);
+                      return null;
+                  }
+              })
               .exceptionally(e -> {
                   logger.error("Request failed", e);
                   return null;
