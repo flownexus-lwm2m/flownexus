04ebcc774be381a5fbcbcb87b89ddb85df328acc
Jonas Remmert <jremmert@gmx.net>
2024-05-22 11:48:14 +0200

    django: Adapt to the new database model
    - Rewrite the serializer for single and multi resource
    - Adapt admin dashboard
    - Remove listView for now, admin dashboard is sufficient for testing
      currently
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/sensordata/views.py
+++ server/django/sensordata/views.py
@@ -1,26 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import GenericLWM2MSerializer
-from .models import SensorData
-from django.views.generic import ListView
+from .serializers import LwM2MSerializer
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class CreateSensorDataView(APIView):
-    def post(self, request, format=None):
-        serializer = GenericLWM2MSerializer(data=request.data)
+    def post(self, request):
+
+        serializer = LwM2MSerializer(data=request.data, many=False)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
         else:
+            print(serializer.errors)
             logger.error(f"Errors: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class SensorDataListView(ListView):
-    model = SensorData
-    template_name = 'timetemperature_list.html'
-    context_object_name = 'timetemperature_list'
-    queryset = SensorData.objects.all()
--- server/django/server/urls.py
+++ server/django/server/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from sensordata import views as sensordata_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sensordata.urls')),
-    path('sensordata/list/', sensordata_views.SensorDataListView.as_view(), name='sensordata_list'),
 ]
