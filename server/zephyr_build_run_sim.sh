a699394101f46e64db70dd4cf097293c06b6f27a
Jonas Remmert <jremmert@gmx.net>
2024-05-25 18:47:36 +0200

    scripts: zephyr: separate from server dir
    Move the zephyr start scripts from server dir to root directory. The
    scripts are used for testing and are not directly related to the server
    implementation.
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/zephyr_build_run_sim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Start networking via net-tools
-../../tools/net-tools/net-setup.sh &
-
-## Build and run Zephyr
-west build -b qemu_x86 ../../zephyr/samples/net/lwm2m_client -- -DCONF=overlay-lwm2m-1.1.conf
-west build -t run
