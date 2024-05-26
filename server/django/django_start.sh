c83806f2e78267f7a8b5a1317b5d8c8f351c015d
Jonas Remmert <jremmert@gmx.net>
2024-05-26 16:37:58 +0200

    django: start script: make migrations for sensordata
    Load initial data when creating a new database.
    
    Do not update static files at each start in the django startup script.
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/django_start.sh
+++ server/django/django_start.sh
@@ -4,7 +4,12 @@ logfile="logs/django_$(date +%Y-%m-%d_%H-%M-%S).log"
 touch "$logfile"
 
 # Start Django Server
-python manage.py collectstatic --noinput 2>&1 | tee -a "$logfile"
-python manage.py makemigrations 2>&1 | tee -a "$logfile"
+# python manage.py collectstatic --noinput 2>&1 | tee -a "$logfile"
+echo "Running makemigrations for sensordata..."
+python manage.py makemigrations sensordata 2>&1 | tee -a "$logfile"
+echo "Running migrate..."
 python manage.py migrate 2>&1 | tee -a $logfile
+echo "Loading initial data..."
+python manage.py loaddata db_initial_data.json 2>&1 | tee -a $logfile
+echo "Starting the server..."
 python manage.py runserver 0.0.0.0:8000 2>&1 | tee -a "$logfile"
