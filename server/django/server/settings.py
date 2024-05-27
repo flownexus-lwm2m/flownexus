fc2c349482eec942bc06cde5be07cad4d47f8802
Jonas Remmert <jremmert@gmx.net>
2024-05-27 19:38:26 +0200

    django: improve logging, add module
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/server/settings.py
+++ server/django/server/settings.py
@@ -31,14 +31,30 @@ CSRF_TRUSTED_ORIGINS = ['https://controlscope.de']
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {module}: {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'simple',
         },
     },
     'loggers': {
         '': {  # Root logger
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'sensordata': {
             'handlers': ['console'],
             'level': 'DEBUG',
             'propagate': False,
