0308cd634164ecbae28c75e01ad2b539a56ee372
Jonas Remmert <jremmert@gmx.net>
2024-06-05 15:22:22 +0200

    django: migrate to new ReST API endpoint
    - Adapt serializer to new format
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/sensordata/views.py
+++ server/django/sensordata/views.py
@@ -7,14 +7,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class CreateSensorDataView(APIView):
-    def post(self, request):
+class PostResourceView(APIView):
 
+    def post(self, request):
         serializer = LwM2MSerializer(data=request.data, many=False)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-        else:
-            print(serializer.errors)
-            logger.error(f"Errors: {serializer.errors}")
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        logger.error(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
