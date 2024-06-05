ade3cb2eec39777565a22bee6be942aa09a19c7b
Jonas Remmert <jremmert@gmx.net>
2024-06-05 15:09:54 +0200

    leshan: clean up, send endpoint events
    Endpoint events (registered, updated, unregistered) are send to django
    via LwM2M user defined resources (10240/0/0|1|2). This is allows to use
    the existing database model in Django.
    
    Build json data with jackson json library instead of manually.
    
    Migrate to new API (leshan_api/resource).
    
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/leshan/src/main/java/lwm2m/DataSenderRest.java
+++ server/leshan/src/main/java/lwm2m/DataSenderRest.java
@@ -5,38 +5,73 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+final class ApiPath {
+    private final String path;
+
+    private ApiPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    /* Static factory method for creating instance of ApiPath */
+    public static final ApiPath RESOURCE = new ApiPath("/leshan_api/resource");
+
+    @Override
+    public String toString() {
+        return path;
+    }
+}
+
 public class DataSenderRest {
     private static final Logger log = LoggerFactory.getLogger(DataSenderRest.class);
     private HttpClient client;
-    private static final String BASE_URI = System.getenv("DATA_SENDER_URI");
+    private ObjectMapper mapper;
+    private static final String BASE_URI = (System.getenv("DATA_SENDER_URI") != null ?
+                                            System.getenv("DATA_SENDER_URI") :
+                                            "http://0.0.0.0:8000");
 
     public DataSenderRest() {
         client = HttpClient.newHttpClient();
+        mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
     }
 
-    public void sendData(String data) {
-        String uri = (BASE_URI != null ? BASE_URI : "http://localhost:8000") +
-                                                    "/api/endpointdata/";
+    public void sendData(ApiPath apiPath, Object jsonData) {
+        /* Prepare request data */
+        HttpRequest request = null;
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            String data = mapper.writeValueAsString(jsonData);
+            request = HttpRequest.newBuilder()
+                     .uri(URI.create(BASE_URI + apiPath.getPath()))
+                     .header("Content-Type", "application/json")
+                     .POST(HttpRequest.BodyPublishers.ofString(data))
+                     .build();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(uri))
-                .header("Content-Type", "application/json")
-                .POST(BodyPublishers.ofString(data))
-                .build();
+            log.trace("POST to {}:\n{}", request.uri(), data);
 
-        log.debug("POST: {}", data);
 
+        } catch (Exception e) {
+            log.error("Error in converting object to JSON", e);
+            return;
+        }
+
+        /* Send request asynchronously */
         client.sendAsync(request, BodyHandlers.ofString())
               .thenApply(response -> {
                   if (response.statusCode() >= 200 && response.statusCode() < 300) {
                       return response.body();
                   } else {
                       log.error("Request failed with: {}", response.statusCode());
-                      log.error("Payload: {}", data);
                       return null;
                   }
               })
