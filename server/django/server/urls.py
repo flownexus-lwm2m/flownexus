d6f243237dddf3663e5f0c6fe310d0f1ad02bf44
Jonas Remmert <j.remmert@phytec.de>
2024-07-23 23:10:40 +0200

    django: add http download server feature
    Added the option to upload a file and register it to the firmware table.
    
    - Check filesize (max 1 MB)
    - Remove explicit name and download field as it is redundant
    - Firmware Version has to be unique
    - Support for multiple files with identical name. This allows to choose
      a binary file like 'merged.bin' for several version. Django
      automatically adds a unique string 'merged_Y7WrC6M.elf' to distinguish
      between files. The download link is the file with the unique string.
    
    Example dl link: https://flownexus.org:8000/media/firmware/v0.1.0_Y7WrC6M.bin
    
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

--- server/django/server/urls.py
+++ server/django/server/urls.py
@@ -6,8 +6,13 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leshan_api/', include('sensordata.urls')),
 ]
+
+# Serve static firmware files
+urlpatterns += static(settings.FIRMWARE_URL, document_root=settings.FIRMWARE_ROOT)
