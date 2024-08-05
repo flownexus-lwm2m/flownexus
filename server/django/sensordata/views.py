a5a6aad27fedf82d7d42542ea092f3fe9d9597c0
Akarshan Kapoor <data.akarshan@icloud.com>
2024-08-06 00:20:23 +0200

    feat: add dashboard templates, version 0.1.0
    This PR successfully replicates the original
    PR to allow for a linear merge history.
    
    Signed-off-by: Akarshan Kapoor <data.akarshan@icloud.com>

--- server/django/sensordata/views.py
+++ server/django/sensordata/views.py
@@ -3,18 +3,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+import traceback, logging
+from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Endpoint, Resource, Event, EndpointOperation, Firmware, ResourceType
 from rest_framework.exceptions import ValidationError
 from .serializers.single_resource_serializer import SingleResourceSerializer
 from .serializers.composite_resource_serializer import CompositeResourceSerializer
-import traceback
-import logging
+from .serializers.generic_resource_serializer import GenericResourceSerializer
 
 logger = logging.getLogger(__name__)
-
 
 class PostSingleResourceView(APIView):
     serializer_class = SingleResourceSerializer
@@ -45,3 +47,58 @@ class PostCompositeResourceView(APIView):
             logger.error("Request data: %s", request.data)
             logger.error("Backtrace: %s", traceback.format_exc())
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ResourceDataView(ListAPIView):
+    serializer_class = GenericResourceSerializer
+
+    def get_queryset(self):
+        resource_name = self.kwargs.get('resource_name')
+        logger.debug(f"Received request for resource type: {resource_name}")
+        try:
+            resource_type = ResourceType.objects.get(name=resource_name)
+            logger.debug(f"Found resource type: {resource_type}")
+            return Resource.objects.filter(resource_type=resource_type)
+        except ResourceType.DoesNotExist:
+            logger.error(f"Resource type {resource_name} not found")
+            return Response({"error": f"{resource_name} resource type not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, *args, **kwargs):
+        resource_name = self.kwargs.get('resource_name')
+        queryset = self.get_queryset()
+
+        if not queryset.exists():
+            logger.error(f"Resource type {resource_name} not found")
+            return Response({"error": f"{resource_name} resource type not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        logger.debug(f"{resource_name.capitalize()} resources: {queryset}")
+        serializer = self.get_serializer(queryset, many=True)
+        logger.debug(f"{resource_name.capitalize()} data: {serializer.data}")
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+@login_required
+def license_dashboard_view(request):
+    return render(request, 'license.html', {'title': 'Project Information and Licenses'})
+
+@login_required
+def admin_dashboard_view(request):
+    devices = Endpoint.objects.all()
+    return render(request, 'admin_dashboard.html', {'devices': devices, 'title': 'Admin Dashboard'})
+
+@login_required
+def firmware_dashboard_view(request):
+    firmware_updates = Firmware.objects.all()
+    return render(request, 'firmware_dashboard.html', {'firmware_updates': firmware_updates, 'title': 'Firmware Update Dashboard'})
+
+@login_required
+def event_dashboard_view(request):
+    events = Event.objects.all()
+    return render(request, 'event_dashboard.html', {'events': events, 'title': 'Event Dashboard'})
+
+@login_required
+def graph_dashboard_view(request):
+    return render(request, 'graph_dashboard.html', {'title': 'Graph Dashboard'})
+
+@login_required
+def pending_communication_dashboard_view(request):
+    pending_operations = EndpointOperation.objects.filter(status='QUEUED')
+    return render(request, 'pending_communication_dashboard.html', {'pending_operations': pending_operations, 'title': 'Pending Communication'})
--- server/django/server/urls.py
+++ server/django/server/urls.py
@@ -6,12 +6,21 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from sensordata import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leshan_api/', include('sensordata.urls')),
+    path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('firmware_dashboard/', views.firmware_dashboard_view, name='firmware_dashboard'),
+    path('event_dashboard/', views.event_dashboard_view, name='event_dashboard'),
+    path('graph_dashboard/', views.graph_dashboard_view, name='graph_dashboard'),
+    path('pending_communication_dashboard/', views.pending_communication_dashboard_view, name='pending_communication_dashboard'),
+    path('license/', views.license_dashboard_view, name='license_dashboard'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.admin_dashboard_view, name='default'),
 ]
 
 # Serve static firmware files
