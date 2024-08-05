a128dc8f9a6da70ae6c211a9858e9ae917dc25c8
Akarshan Kapoor <data.akarshan@icloud.com>
2024-08-06 00:20:23 +0200

    feat: add dashboard templates, version 0.1.0
    This PR successfully replicates the original
    PR to allow for a linear merge history.
    
    Signed-off-by: Akarshan Kapoor <data.akarshan@icloud.com>

--- server/django/sensordata/urls.py
+++ server/django/sensordata/urls.py
@@ -5,10 +5,10 @@
 #
 
 from django.urls import path
-from .views import PostSingleResourceView, PostCompositeResourceView
-
+from .views import PostSingleResourceView, PostCompositeResourceView, ResourceDataView
 
 urlpatterns = [
+    path('data/<str:resource_name>/', ResourceDataView.as_view(), name='resource-data'),
     path('resource/single', PostSingleResourceView.as_view(), name='post-single-resource'),
     path('resource/composite', PostCompositeResourceView.as_view(), name='post-composite-resource'),
 ]
