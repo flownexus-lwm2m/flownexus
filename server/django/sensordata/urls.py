aa529dd89a1e877c1003a1b1cf60c25f3fd5c8ef
Akarshan Kapoor <data.akarshan@icloud.com>
2024-09-02 21:14:40 +0000

    Update: Finalise all generic_resource_modelling
    This commit updates and finalises all dashboard
    files. These include changes in various files like
    serialisers, dashboard and views and urls.
    
    Signed-off-by: Akarshan Kapoor <data.akarshan@icloud.com>

--- server/django/sensordata/urls.py
+++ server/django/sensordata/urls.py
@@ -9,11 +9,17 @@ from .views import (
     PostSingleResourceView,
     PostCompositeResourceView,
     PostTimestampedResourceView,
-    ResourceDataView
+    # ResourceDataView
 )
+from . import views
 
 urlpatterns = [
-    path('data/<str:resource_name>/', ResourceDataView.as_view(), name='resource-data'),
+    path('endpoints/', views.EndpointView.as_view(), name='endpoint-list'),
+    path('endpoints/<str:endpoint_id>/', views.EndpointView.as_view(), name='endpoint-detail'),
+    path('endpoints/<str:endpoint_id>/resources/', views.EndpointResourceView.as_view(), name='endpoint-resource-list'),
+    path('endpoints/<str:endpoint_id>/resources/<int:resource_id>/', views.EndpointResourceView.as_view(), name='endpoint-resource-detail'),
+    path('endpoints/<str:endpoint_id>/firmware/', views.EndpointFirmwareView.as_view(), name='endpoint-firmware'),
+    path('endpoints/<str:endpoint_id>/operations/', views.EndpointOperationView.as_view(), name='endpoint-operations'),
     path('resource/single', PostSingleResourceView.as_view(), name='post-single-resource'),
     path('resource/composite', PostCompositeResourceView.as_view(), name='post-composite-resource'),
     path('resource/timestamped', PostTimestampedResourceView.as_view(), name='post-timestamped-resource'),
