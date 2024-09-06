eba215533f82500da18e1293884d6d7ee7e7b4e6
Jonas Remmert <jremmert@gmx.net>
2024-09-06 16:15:29 +0200

    django: fix resource serializer tests
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/sensordata/tests/test_post_single_resource.py
+++ server/django/sensordata/tests/test_post_single_resource.py
@@ -7,12 +7,14 @@
 from django.urls import reverse
 from rest_framework import status
 from django.test import TestCase
+import json
 from sensordata.models import (
     ResourceType,
     Resource,
 )
 
-TEST_PAYLOAD = {
+TEST_PAYLOAD = '''
+{
         "ep": "qemu_x86",
         "obj_id": 3303,
         "val": {
@@ -22,15 +24,17 @@ TEST_PAYLOAD = {
             "value": "24.899181214836236"
         }
 }
+'''
 
 class SensorDataTests(TestCase):
 
     def setUp(self):
         # Ensure that the ResourceType object exists
         ResourceType.objects.create(object_id=3303, resource_id=5700,
-                                    name="temperature", data_type="float_value")
+                                    name="temperature", data_type=ResourceType.FLOAT)
 
     def test_post_single_resource(self):
+        PL_DICT = json.loads(TEST_PAYLOAD)
         url = reverse('post-single-resource')
 
         response = self.client.post(url, TEST_PAYLOAD, content_type='application/json')
@@ -40,15 +44,14 @@ class SensorDataTests(TestCase):
 
         # Query the database to retrieve the created object
         created_object = Resource.objects.get(
-            resource_type__object_id = TEST_PAYLOAD["obj_id"],
-            resource_type__resource_id = TEST_PAYLOAD["val"]["id"],
-            endpoint__endpoint= TEST_PAYLOAD["ep"]
+            resource_type__object_id = PL_DICT["obj_id"],
+            resource_type__resource_id = PL_DICT["val"]["id"],
+            endpoint__endpoint= PL_DICT["ep"]
         )
 
         # Compare the retrieved object's attributes to the expected values
-        self.assertEqual(created_object.endpoint.endpoint, TEST_PAYLOAD["ep"])
-        self.assertEqual(created_object.resource_type.object_id, TEST_PAYLOAD["obj_id"])
-        self.assertEqual(created_object.resource_type.resource_id, TEST_PAYLOAD["val"]["id"])
-
-        self.assertEqual(created_object.resource_type.data_type, "float_value")
-        self.assertEqual(created_object.float_value, float(TEST_PAYLOAD["val"]["value"]))
+        self.assertEqual(created_object.endpoint.endpoint, PL_DICT["ep"])
+        self.assertEqual(created_object.resource_type.object_id, PL_DICT["obj_id"])
+        self.assertEqual(created_object.resource_type.resource_id, PL_DICT["val"]["id"])
+        self.assertEqual(created_object.resource_type.data_type, ResourceType.FLOAT)
+        self.assertEqual(created_object.float_value, float(PL_DICT["val"]["value"]))
