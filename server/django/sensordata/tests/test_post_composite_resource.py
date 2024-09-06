225289a0ace6e883b02df2bcb54d1ca3ab4c97a6
Jonas Remmert <j.remmert@phytec.de>
2024-09-06 16:15:29 +0200

    django: fix resource serializer tests
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

--- server/django/sensordata/tests/test_post_composite_resource.py
+++ server/django/sensordata/tests/test_post_composite_resource.py
@@ -11,185 +11,173 @@ from sensordata.models import (
     ResourceType,
 )
 
-TEST_PAYLOAD = {
-  "ep" : "qemu_x86",
+TEST_PAYLOAD = '''
+{
+  "ep" : "urn:imei:100000000000000",
   "val" : {
-    "instances" : [ {
-      "kind" : "instance",
-      "resources" : [ {
-        "kind" : "singleResource",
-        "id" : 0,
-        "type" : "STRING",
-        "value" : "Zephyr"
-      }, {
-        "kind" : "singleResource",
-        "id" : 1,
-        "type" : "STRING",
-        "value" : "OMA-LWM2M Sample Client"
-      }, {
-        "kind" : "singleResource",
-        "id" : 2,
-        "type" : "STRING",
-        "value" : "345000123"
-      }, {
-        "kind" : "singleResource",
-        "id" : 3,
-        "type" : "STRING",
-        "value" : "1.0"
-      }, {
-        "kind" : "multiResource",
-        "values" : {
-          "0" : "1",
-          "1" : "5"
-        },
-        "id" : 6,
-        "type" : "INTEGER"
-      }, {
-        "kind" : "multiResource",
-        "values" : {
-          "0" : "3800",
-          "1" : "5000"
-        },
-        "id" : 7,
-        "type" : "INTEGER"
-      }, {
-        "kind" : "multiResource",
-        "values" : {
-          "0" : "125",
-          "1" : "900"
-        },
-        "id" : 8,
-        "type" : "INTEGER"
-      }, {
-        "kind" : "singleResource",
-        "id" : 9,
-        "type" : "INTEGER",
-        "value" : "95"
-      }, {
-        "kind" : "singleResource",
-        "id" : 10,
-        "type" : "INTEGER",
-        "value" : "15"
-      }, {
-        "kind" : "multiResource",
-        "values" : {
-          "0" : "0"
-        },
-        "id" : 11,
-        "type" : "INTEGER"
-      }, {
-        "kind" : "singleResource",
-        "id" : 13,
-        "type" : "TIME",
-        "value" : 2000
-      }, {
-        "kind" : "singleResource",
-        "id" : 14,
-        "type" : "STRING",
-        "value" : ""
-      }, {
-        "kind" : "singleResource",
-        "id" : 15,
-        "type" : "STRING",
-        "value" : ""
-      }, {
-        "kind" : "singleResource",
-        "id" : 16,
-        "type" : "STRING",
-        "value" : "U"
-      }, {
-        "kind" : "singleResource",
-        "id" : 17,
-        "type" : "STRING",
-        "value" : "qemu_x86"
-      }, {
-        "kind" : "singleResource",
-        "id" : 18,
-        "type" : "STRING",
-        "value" : "1.0.1"
-      }, {
-        "kind" : "singleResource",
-        "id" : 19,
-        "type" : "STRING",
-        "value" : ""
-      }, {
-        "kind" : "singleResource",
-        "id" : 20,
-        "type" : "INTEGER",
-        "value" : "1"
-      }, {
-        "kind" : "singleResource",
-        "id" : 21,
-        "type" : "INTEGER",
-        "value" : "25"
+    "/3" : {
+      "instances" : [ {
+        "kind" : "instance",
+        "resources" : [ {
+          "kind" : "singleResource",
+          "id" : 0,
+          "type" : "STRING",
+          "value" : "Zephyr"
+        }, {
+          "kind" : "singleResource",
+          "id" : 1,
+          "type" : "STRING",
+          "value" : "OMA-LWM2M Sample Client"
+        }, {
+          "kind" : "singleResource",
+          "id" : 2,
+          "type" : "STRING",
+          "value" : "345000123"
+        }, {
+          "kind" : "singleResource",
+          "id" : 3,
+          "type" : "STRING",
+          "value" : "1.0"
+        }, {
+          "kind" : "multiResource",
+          "values" : {
+            "0" : "1",
+            "1" : "5"
+          },
+          "id" : 6,
+          "type" : "INTEGER"
+        }, {
+          "kind" : "multiResource",
+          "values" : {
+            "0" : "3800",
+            "1" : "5000"
+          },
+          "id" : 7,
+          "type" : "INTEGER"
+        }, {
+          "kind" : "multiResource",
+          "values" : {
+            "0" : "125",
+            "1" : "900"
+          },
+          "id" : 8,
+          "type" : "INTEGER"
+        }, {
+          "kind" : "singleResource",
+          "id" : 9,
+          "type" : "INTEGER",
+          "value" : "95"
+        }, {
+          "kind" : "singleResource",
+          "id" : 10,
+          "type" : "INTEGER",
+          "value" : "15"
+        }, {
+          "kind" : "multiResource",
+          "values" : {
+            "0" : "0"
+          },
+          "id" : 11,
+          "type" : "INTEGER"
+        }, {
+          "kind" : "singleResource",
+          "id" : 13,
+          "type" : "TIME",
+          "value" : 0
+        }, {
+          "kind" : "singleResource",
+          "id" : 14,
+          "type" : "STRING",
+          "value" : ""
+        }, {
+          "kind" : "singleResource",
+          "id" : 15,
+          "type" : "STRING",
+          "value" : ""
+        }, {
+          "kind" : "singleResource",
+          "id" : 16,
+          "type" : "STRING",
+          "value" : "U"
+        }, {
+          "kind" : "singleResource",
+          "id" : 17,
+          "type" : "STRING",
+          "value" : "native_sim"
+        }, {
+          "kind" : "singleResource",
+          "id" : 18,
+          "type" : "STRING",
+          "value" : "1.0.1"
+        }, {
+          "kind" : "singleResource",
+          "id" : 19,
+          "type" : "STRING",
+          "value" : ""
+        }, {
+          "kind" : "singleResource",
+          "id" : 20,
+          "type" : "INTEGER",
+          "value" : "1"
+        }, {
+          "kind" : "singleResource",
+          "id" : 21,
+          "type" : "INTEGER",
+          "value" : "25"
+        } ],
+        "id" : 0
       } ],
-      "id" : 0
-    } ],
-    "kind" : "obj",
-    "id" : 3
+      "kind" : "obj",
+      "id" : 3
+    }
   }
 }
+'''
+
 
 class SensorDataTests(TestCase):
 
     def setUp(self):
         # Ensure that all ResourceTypes object exist
         ResourceType.objects.create(object_id=3, resource_id=0,
-                                    name="Manufacturer", data_type="str_value")
-
+                                    name="Manufacturer", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=1,
-                                    name="Model Number", data_type="str_value")
-
+                                    name="Model Number", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=2,
-                                    name="Serial Number", data_type="str_value")
-
+                                    name="Serial Number", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=3,
-                                    name="Firmware Version", data_type="str_value")
-
+                                    name="Firmware Version", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=6,
-                                    name="Supported Objects", data_type="int_value")
-
+                                    name="Supported Objects", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=7,
-                                    name="Supported Resources", data_type="int_value")
-
+                                    name="Supported Resources", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=8,
-                                    name="Manufacturer ID", data_type="int_value")
-
+                                    name="Manufacturer ID", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=9,
-                                    name="Memory Free", data_type="int_value")
-
+                                    name="Memory Free", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=10,
-                                    name="Error Code", data_type="int_value")
-
+                                    name="Error Code", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=11,
-                                    name="Current Time", data_type="int_value")
-
+                                    name="Current Time", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=13,
-                                    name="UTC Offset", data_type="int_value")
-
+                                    name="UTC Offset", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=14,
-                                    name="Timezone", data_type="str_value")
-
+                                    name="Timezone", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=15,
-                                    name="Supported Binding and Modes", data_type="str_value")
-
+                                    name="Supported Binding and Modes",
+                                    data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=16,
-                                    name="Device Type", data_type="str_value")
-
+                                    name="Device Type", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=17,
-                                    name="Hardware Version", data_type="str_value")
-
+                                    name="Hardware Version", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=18,
-                                    name="Software Version", data_type="str_value")
-
+                                    name="Software Version", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=19,
-                                    name="Battery Level", data_type="str_value")
-
+                                    name="Battery Level", data_type=ResourceType.STRING)
         ResourceType.objects.create(object_id=3, resource_id=20,
-                                    name="Battery Status", data_type="int_value")
-
+                                    name="Battery Status", data_type=ResourceType.INTEGER)
         ResourceType.objects.create(object_id=3, resource_id=21,
-                                    name="Memory Total", data_type="int_value")
+                                    name="Memory Total", data_type=ResourceType.INTEGER)
 
 
     def test_post_composite_resource(self):
