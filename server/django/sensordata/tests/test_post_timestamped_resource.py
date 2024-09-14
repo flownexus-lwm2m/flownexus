8a0a792481e0c6629bc90e8d85cf677a55b366a4
Jonas Remmert <jremmert@gmx.net>
2024-09-14 09:32:58 +0200

    squash: django: test: timestamped resource
    Signed-off-by: Jonas Remmert <jremmert@gmx.net>

--- server/django/sensordata/tests/test_post_timestamped_resource.py
+++ server/django/sensordata/tests/test_post_timestamped_resource.py
@@ -13,25 +13,63 @@ from sensordata.models import (
 
 TEST_PAYLOAD = '''
 {
-  "ep" : "urn:imei:100000000000000",
-  "val" : {
-    "empty" : false,
-    "timestamps" : [ null ],
-    "nodes" : {
-      "/3303/0/5700" : {
-        "kind" : "singleResource",
-        "id" : 5700,
-        "type" : "FLOAT",
-        "value" : "22.19110046564394"
-      },
-      "/3304/0/5700" : {
-        "kind" : "singleResource",
-        "id" : 5700,
-        "type" : "FLOAT",
-        "value" : "52.237076014801175"
+  "ep": "urn:imei:100000000000000",
+  "val": [
+    {
+      "null": {
+        "nodes": {
+          "/3303/0/5700": {
+            "kind": "singleResource",
+            "id": 5700,
+            "type": "FLOAT",
+            "value": "20.145982801948204"
+          },
+          "/3304/0/5700": {
+            "kind": "singleResource",
+            "id": 5700,
+            "type": "FLOAT",
+            "value": "51.200783720985235"
+          }
+        }
+      }
+    },
+    {
+      "2024-09-13T19:06:17.032059888Z": {
+        "nodes": {
+          "/10300/0/0": {
+            "kind": "singleResource",
+            "id": 0,
+            "type": "INTEGER",
+            "value": "8"
+          }
+        }
+      }
+    },
+    {
+      "2024-09-13T19:06:18.032059888Z": {
+        "nodes": {
+          "/10300/0/0": {
+            "kind": "singleResource",
+            "id": 0,
+            "type": "INTEGER",
+            "value": "9"
+          }
+        }
+      }
+    },
+    {
+      "2024-09-13T19:06:19.032059888Z": {
+        "nodes": {
+          "/10300/0/0": {
+            "kind": "singleResource",
+            "id": 0,
+            "type": "INTEGER",
+            "value": "10"
+          }
+        }
       }
     }
-  }
+  ]
 }
 '''
 
@@ -44,6 +82,8 @@ class SensorDataTests(TestCase):
                                     name="temperature", data_type=ResourceType.FLOAT)
         ResourceType.objects.create(object_id=3304, resource_id=5700,
                                     name="humidity", data_type=ResourceType.FLOAT)
+        ResourceType.objects.create(object_id=10300, resource_id=0,
+                                    name="humidity", data_type=ResourceType.INTEGER)
 
 
     def test_post_composite_resource(self):
