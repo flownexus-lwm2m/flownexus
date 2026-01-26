eb86581bbc4240aa8ff8d1fb5fd0f41acb2162cc
Jonas Remmert <j.remmert@mailbox.org>
2026-01-27 00:33:22 +0100

    frontend: extract UI logic from sensordata app
    Separate user-facing dashboards and templates from the core sensor data
    API. This improves the project structure by decoupling the data
    ingestion layer from the visualization layer.
    
    - Move dashboard templates to frontend/templates
    - Move dashboard views to frontend/views.py
    - Refactor sensordata/views.py to be API-only
    - Register frontend app and update routing
    
    Signed-off-by: Jonas Remmert <j.remmert@mailbox.org>

---
+++ server/django/frontend/apps.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2024 Jonas Remmert
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from django.apps import AppConfig
+
+
+class FrontendConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "frontend"
--- server/django/server/settings.py
+++ server/django/server/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "drf_spectacular",
     "sensordata",
+    "frontend",
 ]
 
 MIDDLEWARE = [
