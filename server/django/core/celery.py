05bdaa52662ab03832544802222623753d3f4f5e
Jonas Remmert <j.remmert@mailbox.org>
2026-01-27 00:50:36 +0100

    server: rename project configuration directory to core
    Rename the redundant 'server' sub-directory to 'core' to better reflect
    its role as the project's configuration hub. Additionally:
    
    - Point manage.py, wsgi, asgi, and celery to the new 'core' namespace
    - Update MEDIA_ROOT and MEDIA_URL in settings.py for better portability
    - Update Celery worker startup command in django_start.sh
    
    Signed-off-by: Jonas Remmert <j.remmert@mailbox.org>

--- server/django/core/celery.py
+++ server/django/core/celery.py
@@ -8,9 +8,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('server')
+app = Celery('core')
 
 app.conf.broker_connection_retry_on_startup = True
 
