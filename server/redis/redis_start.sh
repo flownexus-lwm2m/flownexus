73aa7e44ac2fd11401035345f9e6b0ac71b43ea2
Jonas Remmert <j.remmert@mailbox.org>
2026-01-27 01:27:59 +0100

    Redis: Fix startup script to background server and allow health check
    Signed-off-by: Jonas Remmert <j.remmert@mailbox.org>

--- server/redis/redis_start.sh
+++ server/redis/redis_start.sh
@@ -6,16 +6,16 @@ mkdir -p logs
 logfile="logs/redis_$(date +%Y-%m-%d_%H-%M-%S).log"
 touch "$logfile"
 
-# Start Redis server
-redis-server /redis/redis.conf  2>&1 | tee -a $logfile
+# Start Redis server in the background to allow the health check loop to run
+redis-server /redis/redis.conf 2>&1 | tee -a "$logfile" &
 
 # Wait for Redis to start
-until redis-cli ping; do
+until redis-cli ping > /dev/null 2>&1; do
     echo "Waiting for Redis to start..."
     sleep 1
 done
 
 echo "Redis is running."
 
-# Wait for Redis to exit
+# Wait for the background process to exit
 wait
