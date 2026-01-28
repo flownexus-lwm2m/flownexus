75eb5b07179bdf1e5cd5c6057be4df2a4b0b2090
Jonas Remmert <j.remmert@mailbox.org>
2026-01-28 12:27:22 +0100

    Frontend: Implement new Tabler-based dashboard
    Signed-off-by: Jonas Remmert <j.remmert@mailbox.org>

--- server/django/frontend/urls.py
+++ server/django/frontend/urls.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2024 Jonas Remmert
+# Copyright (c) 2026 Jonas Remmert
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,12 +7,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'frontend'
+
 urlpatterns = [
-    path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
-    path('device_dashboard/', views.device_dashboard_view, name='device_dashboard'),
-    path('event_dashboard/', views.event_dashboard_view, name='event_dashboard'),
-    path('firmware_dashboard/', views.firmware_dashboard_view, name='firmware_dashboard'),
-    path('license/', views.license_dashboard_view, name='license_dashboard'),
-    path('download_csv/', views.download_csv, name='download_csv'),
-    path('', views.admin_dashboard_view, name='default'),
+    path('', views.dashboard, name='dashboard'),
 ]
